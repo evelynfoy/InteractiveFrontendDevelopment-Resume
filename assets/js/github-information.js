0dfe70033b758e7632b80e0e9376cf28fca78aee
evelynfoy <evelyn.foy@hotmail.com>
2022-03-08 18:18:06 +0000

    Fix bugs for the gh-user-data & gh-repo-data elements

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -37,11 +37,15 @@ function repoInformationHTML(repos){
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
+
     var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
         return;
     }
+
     $("#gh-user-data").html(
         `<div id="loader">
             <img src="assets/css/loader.gif" alt="loading..." />
@@ -68,3 +72,4 @@ function fetchGitHubInformation(event) {
     )
 }
 
+$(document).ready(fetchGitHubInformation);
